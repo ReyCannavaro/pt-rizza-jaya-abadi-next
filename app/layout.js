8ef4c4a9f76ba349b868d3a0cc1a7379b18aa27f
Rey Cannavaro <reyjunoalcannavaro@gmail.com>
2026-05-30 11:09:51 +0700

    fix bug on layout.js

--- app/layout.js
+++ app/layout.js
@@ -13,7 +13,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="id">
+    <html lang="id" data-scroll-behavior="smooth">
       <body className={inter.className}>
         <Navbar />
         <main>{children}</main>
